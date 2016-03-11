cfff0635b25294a890a6990021659ca6933e643f
jonechenug <jonechenug@gmail.com>
2016-03-11 23:08:07 +0800

    Update fs_start.sh

--- fs_start.sh
+++ fs_start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-logname=server.log
 cd /fs/
 nohup java -Xmx512M -XX:MaxGCPauseMillis=30 -jar fs.jar > $logname 2>&1  &
-echo FinalSpeed start,log file: /fs/$logname
+echo FinalSpeed start,log file: /fs/server.log
 
