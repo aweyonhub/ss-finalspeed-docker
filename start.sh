da71a8860365e03795937e0c46401bead9bbe91a
jonechenug <jonechenug@gmail.com>
2016-03-13 10:45:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,3 +8,5 @@ echo " Please remember the password!"
 echo "========================================================================"
 
 ssserver -s $SS_SERVER_ADDR -p $SS_SERVER_PORT -k $SS_PASSWORD -m $SS_METHOD -t $SS_TIMEOUT
+ufw allow $SS_SERVER_PORT
+ufw enable
