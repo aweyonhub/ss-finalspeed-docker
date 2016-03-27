80b31a0486231b40a1223986c89d38b1880440c9
jonechenug <jonechenug@gmail.com>
2016-03-27 17:09:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 echo "========================================================================"
 echo " You can now connect to this ShadowSocks server:"
 echo ""
-echo " server: $SS_SERVER_ADDR  port: $SS_SERVER_PORT password: $SS_PASSWORD  "
+echo " server: $SS_SERVER_ADDR  port: 8388 password: $SS_PASSWORD  "
 echo " timeout: $SS_TIMEOUT  encryption method: $SS_METHOD "
 echo ""
 echo " Please remember the password!"
 echo "========================================================================"
 
-ssserver -s $SS_SERVER_ADDR -p $SS_SERVER_PORT -k $SS_PASSWORD -m $SS_METHOD -t $SS_TIMEOUT &
-sudo ufw allow $SS_SERVER_PORT
+ssserver -s $SS_SERVER_ADDR -p 8388 -k $SS_PASSWORD -m $SS_METHOD -t $SS_TIMEOUT
